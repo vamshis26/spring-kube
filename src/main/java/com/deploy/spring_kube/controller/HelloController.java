907f6e648647bd1f3bc013f198858a1e011aa59b
Vamshi <Vamshi@Vamshis-MacBook-Air.local>
2026-07-14 14:35:22 -0500

    Changed route

--- src/main/java/com/deploy/spring_kube/controller/HelloController.java
+++ src/main/java/com/deploy/spring_kube/controller/HelloController.java
@@ -3,15 +3,19 @@ package com.deploy.spring_kube.controller;
 import com.deploy.spring_kube.exceptions.UserNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("hello-kubernetes")
-    public String getMessage(){
-        return "Hello, We successfully deployed to K8s, now you are seeing version 3.0";
+
+    @GetMapping("/user/{name}")
+    public String getMessage(@PathVariable String name){
+        return "Hello "+ name.toUpperCase() + ", We successfully deployed to K8s, now you are seeing version 3.0";
     }
-    @GetMapping("/user/{id}")
+
+    @GetMapping("/users/{id}")
     public String getUser(@PathVariable Long id) {
 
         if(id <= 0) {
